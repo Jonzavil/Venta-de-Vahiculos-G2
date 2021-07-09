9b1655fb494d7bad49dcfbabceae7df1db205a51
Jonzavil <jonzavil@espol.edu.ec>
2021-07-08 21:47:21 -0500

    aumento de clases cendedor y comprador

---
+++ src/main/java/ec/edu/espol/model/Vendedor.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.model;
+
+/**
+ *
+ * @author ZavalaAvila
+ */
+public class Vendedor {
+    private String id;
+    private String nombre;
+    private String apellidos;
+    private String organizacion;
+    private String correoElectronico;
+    private String clave;    
+
+    public Vendedor(String id, String nombre, String apellidos, String organizacion, String correoElectronico, String clave) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.organizacion = organizacion;
+        this.correoElectronico = correoElectronico;
+        this.clave = clave;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getOrganizacion() {
+        return organizacion;
+    }
+
+    public void setOrganizacion(String organizacion) {
+        this.organizacion = organizacion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+}
